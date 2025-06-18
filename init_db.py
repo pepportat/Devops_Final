b4ab99f9c017743ae8bd73c57a2dc231832bd1a2
kiu_bedukadze.lazare <bedukadze.lazare@kiu.edu.ge>
2025-06-19 02:34:08 +0400

    added close_connection method, corrected others

--- init_db.py
+++ init_db.py
@@ -4,69 +4,87 @@ import  psycopg2
 from flask.cli import load_dotenv
 
 class DB_Connection:
-    load_dotenv()
-    connection = psycopg2.connect(
-        host = "localhost",
-        database ="ToDoApp_DB",
-        user = os.getenv('DB_USERNAME'),
-        password = os.getenv('DB_PASSWORD')
-    )
-    cursor =connection.cursor()
+
+    def __init__(self):
+        load_dotenv()
+        self.connection = self.get_connection()
+        self.cursor = self.connection.cursor()
+
+    def get_connection(self):
+        return psycopg2.connect(
+            host = "localhost",
+            database ="ToDoApp_DB",
+            user = os.getenv('DB_USERNAME'),
+            password = os.getenv('DB_PASSWORD')
+        )
+
+    cursor =get_connection().cursor()
 
     def insert(self, title, description, is_done, created_on=datetime.now()):
         self.cursor.execute(
             "INSERT INTO task (title, description, is_done, created_on) values (%s, %s, %s, %s)",
             (title, description, is_done, created_on)
         )
+        self.connection.commit()
 
     def get_by_id(self, task_id):
         self.cursor.execute(
             'SELECT * FROM task WHERE task_id = %s',
-            task_id
+            (task_id,)
         )
+        self.connection.commit()
 
     def get_all(self):
         self.cursor.execute('SELECT * FROM task')
+        return self.cursor.fetchall()
+
 
     def get_by_title(self, title):
         self.cursor.execute(
             'SELECT * FROM task WHERE title = %s',
-            title
+            (title,)
         )
+        self.connection.commit()
+
 
     def delete_by_id(self, task_id):
         self.cursor.execute(
             'DELETE FROM task WHERE task_id = %S',
-            task_id
+            (task_id,)
         )
+        self.connection.commit()
 
     def delete_by_title(self, title):
         self.cursor.execute(
-            'DELETE FROM task WHERE title = %S',
-            title
+            'DELETE FROM task WHERE title = %s',
+            (title,)
         )
+        self.connection.commit()
 
-cur = DB_Connection.cursor
+    def create_table(self):
+        self.cursor.execute('DROP TABLE IF EXISTS task;')
+        self.cursor.execute('''
+                        CREATE TABLE task (
+                        task_id serial PRIMARY KEY,
+                        title VARCHAR(50) NOT NULL UNIQUE,
+                        description VARCHAR(250) NOT NULL,
+                        is_done BOOLEAN,
+                        created_on TIMESTAMP);
+                    ''')
+        self.connection.commit()
 
-cur.execute('DROP TABLE IF EXISTS task;')
-cur.execute('''
-                CREATE TABLE task (
-                task_id serial PRIMARY KEY,
-                title VARCHAR(50) NOT NULL UNIQUE,
-                description VARCHAR(250) NOT NULL,
-                is_done BOOLEAN,
-                created_on TIMESTAMP);
-            ''')
-cur.execute('INSERT INTO task (title, description, is_done, created_on)'
-            'VALUES (%s, %s, %s, %s);',
-            (
-                'Devops Final',
-                'Create a project, which summarizes all the topics we\'ve learned this semester into one big assignment',
-                False,
-                datetime(2025,6,18)
-                )
-            )
-DB_Connection.connection.commit()
+    def seed_table(self):
+        self.cursor.execute('INSERT INTO task (title, description, is_done, created_on)'
+                    'VALUES (%s, %s, %s, %s);',
+                    (
+                        'Devops Final',
+                        'Create a project, which summarizes all the topics we\'ve learned this semester into one big assignment',
+                        False,
+                        datetime(2025, 6, 18)
+                    )
+                    )
+        self.connection.commit()
 
-cur.close()
-DB_Connection.connection.close()
+    def close_connection(self):
+        self.cursor.close()
+        self.connection.close()
